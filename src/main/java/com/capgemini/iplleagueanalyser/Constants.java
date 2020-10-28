9534b9308dc7d7867e73ecfad6b5e2351cee8ae4
abhineetpratyush <abhineetpratyush1998@gmail.com>
2020-10-28 20:00:17 +0530

    [abhineet] add : added test cases to check for various exceptions

--- src/main/java/com/capgemini/iplleagueanalyser/Constants.java
+++ src/main/java/com/capgemini/iplleagueanalyser/Constants.java
@@ -3,4 +3,6 @@ package com.capgemini.iplleagueanalyser;
 public class Constants {
 	public static final String BATSMEN_CSV_FILE_PATH = "./src/main/resources/IPL2019BatsmenData.csv";
 	public static final String BOWLERS_CSV_FILE_PATH = "./src/main/resources/IPL2019BowlersData.csv";
+	public static final String INCORRECT_BATSMAN_CSV_FILE_PATH = "./src/main/resources/_IPL2019BatsmenData.csv";
+	public static final String INCORRECT_HEADER_BATSMAN_CSV_FILE_PATH = "./src/main/resources/IPL2019BatsmenDataIncorrectHeader.csv";
 }
--- src/test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
+++ src/test/java/com/capgemini/iplleagueanalyser/IPLAnalyserTest.java
@@ -4,8 +4,8 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
 import com.capgemini.opencsvbuilder.CustomCSVBuilderException;
+import com.capgemini.opencsvbuilder.ExceptionType;
 import com.opencsv.bean.HeaderColumnNameMappingStrategy;
 import com.opencsv.bean.MappingStrategy;
 
@@ -17,7 +17,15 @@ public class IPLAnalyserTest {
 	public void initialiseIPLAnalyserObject() {
 		iplAnalyser = new IPLAnalyser();
 	}
-
+	
+	@Test
+	public void givenIPLBatsmenData_ShouldReturnNoOfBatsmen() throws CustomFileIOException, CustomCSVBuilderException {
+		MappingStrategy<BatsmenDataStructure> mappingStrategy = new HeaderColumnNameMappingStrategy<BatsmenDataStructure>();
+		mappingStrategy.setType(BatsmenDataStructure.class);
+		int numOfBatsmen = iplAnalyser.loadIPLBatsmenData(Constants.BATSMEN_CSV_FILE_PATH, mappingStrategy, BatsmenDataStructure.class, ',');
+		Assert.assertEquals(100, numOfBatsmen);
+	}
+	
 	@Test
 	public void givenIPLBatsmenData_ShouldReturnSortedDescendingOnBattingAverage() throws CustomFileIOException, CustomCSVBuilderException {
 		MappingStrategy<BatsmenDataStructure> mappingStrategy = new HeaderColumnNameMappingStrategy<BatsmenDataStructure>();
@@ -26,5 +34,55 @@ public class IPLAnalyserTest {
 		List<BatsmenDataStructure> sortedBatsmenListOnAverageDescending = iplAnalyser.getBatsmenListSortedOnAverageDescending();
 		Assert.assertEquals("MS Dhoni", sortedBatsmenListOnAverageDescending.get(0).getPlayerName());
 		Assert.assertEquals("David Warner", sortedBatsmenListOnAverageDescending.get(1).getPlayerName());
+	}
+	
+	@Test
+	public void givenIncorrectCSVFile_ShouldReturnCustomException() throws CustomCSVBuilderException {
+		String exceptionMessage = null;
+		try {
+			MappingStrategy<BatsmenDataStructure> mappingStrategy = new HeaderColumnNameMappingStrategy<BatsmenDataStructure>();
+			mappingStrategy.setType(BatsmenDataStructure.class);
+			iplAnalyser.loadIPLBatsmenData(Constants.INCORRECT_BATSMAN_CSV_FILE_PATH, mappingStrategy, BatsmenDataStructure.class, ',');
+		} catch(CustomFileIOException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionTypeIO.FILE_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenIncorrectCSVType_ShouldReturnCustomException() throws CustomFileIOException {
+		String exceptionMessage = null;
+		try {
+			iplAnalyser.loadIPLBatsmenData(Constants.BATSMEN_CSV_FILE_PATH, null, null, ',');
+		} catch(CustomCSVBuilderException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.PARSE_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenCorrectCSVFileIncorrectDelimiter_ShouldReturnCustomException() throws CustomFileIOException {
+		String exceptionMessage = null;
+		try {
+			MappingStrategy<BatsmenDataStructure> mappingStrategy = new HeaderColumnNameMappingStrategy<BatsmenDataStructure>();
+			mappingStrategy.setType(BatsmenDataStructure.class);
+			iplAnalyser.loadIPLBatsmenData(Constants.BATSMEN_CSV_FILE_PATH, mappingStrategy, BatsmenDataStructure.class, '|');
+		} catch(CustomCSVBuilderException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.HEADER_OR_DELIMITER_PROBLEM.toString(), exceptionMessage);
+	}
+
+	@Test
+	public void givenCorrectCSVFileIncorrectHeader_ShouldReturnCustomException() throws CustomFileIOException {
+		String exceptionMessage = null;
+		try {
+			MappingStrategy<BatsmenDataStructure> mappingStrategy = new HeaderColumnNameMappingStrategy<BatsmenDataStructure>();
+			mappingStrategy.setType(BatsmenDataStructure.class);
+			iplAnalyser.loadIPLBatsmenData(Constants.INCORRECT_HEADER_BATSMAN_CSV_FILE_PATH, mappingStrategy, BatsmenDataStructure.class, ',');
+		} catch(CustomCSVBuilderException e) {
+			exceptionMessage = e.getMessage();
+		}
+		Assert.assertEquals(ExceptionType.HEADER_OR_DELIMITER_PROBLEM.toString(), exceptionMessage);
 	}
 }
